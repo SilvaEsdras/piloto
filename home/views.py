73817e371f7498452f9be21aed744f81bb385ba4
SilvaEsdras <esdrasesportivo@gmail.com>
2025-11-28 15:57:04 -0300

    Ajuste nas rotas

--- home/views.py
+++ home/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# View criada nos slides anteriores
+def index(request):
+    return HttpResponse("A view index funcionou, Wow!")
+
+# Exercício: Criar view 'sobre' com tag h1 [cite: 3591]
+def sobre(request):
+    return HttpResponse("<h1>Sobre o Sistema</h1>")
+
+# Exercício: Criar view 'contato' [cite: 3597]
+def contato(request):
+    return HttpResponse("Esta é a página de contato.")
+
+# Exercício: Criar view 'ajuda' [cite: 3599]
+def ajuda(request):
+    return HttpResponse("Esta é a página de ajuda.")
--- pweb/urls.py
+++ pweb/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Alterar de 'home/' para '' define a raiz do site para a app home
+    path('', include('home.urls')), 
 ]
